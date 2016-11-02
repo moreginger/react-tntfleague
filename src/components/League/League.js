385c95d439c95ff018dd79adb8fe04be60f13c11
Tim Moore <tmm@corefiling.com>
2016-11-02 17:34:07 +0000

    OMG tabs!

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -1,87 +1,87 @@
 import { LeagueActions } from '../../actions/LeagueActions';
 import { LeagueStore } from '../../stores/LeagueStore';
 
-import React, {	PropTypes, Component } from 'react';
+import React, {  PropTypes, Component } from 'react';
 import { Nav, NavItem, Grid, Row } from 'react-bootstrap';
 import Divisions from '../Divisions';
 import AllTime from '../AllTime';
 
 class League extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			tab: 1,
-			data: null
-		};
-		this.updateFromStore();
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: 1,
+      data: null
+    };
+    this.updateFromStore();
+  }
 
-	getTotalWins(seasons, division) {
-		let allTime = new Map();
-		seasons.slice(1).forEach(x => {
-			let inDivision = x.table.filter(p => p.division === division);
-			if (inDivision.length >= 10) { // Arbitrary. This code should move to the data service.
-				let winner = inDivision[0].name;
-				let r = allTime.has(winner) ? allTime.get(winner) : { name: winner, wins: 0};
-				r.wins += 1;
-				r.lastWin = x.date.year * 12 + x.date.month;
-				allTime.set(winner, r);
-			}
-		});
-		allTime = Array.from(allTime.values());
-		allTime.sort((a, b) => {
-			let winsDiff = b.wins - a.wins;
-			if (winsDiff !== 0) {
-				return winsDiff;
-			}
-			return a.lastWin - b.lastWin;
-		});
-		return allTime;
-	}
+  getTotalWins(seasons, division) {
+    let allTime = new Map();
+    seasons.slice(1).forEach(x => {
+      let inDivision = x.table.filter(p => p.division === division);
+      if (inDivision.length >= 10) { // Arbitrary. This code should move to the data service.
+        let winner = inDivision[0].name;
+        let r = allTime.has(winner) ? allTime.get(winner) : { name: winner, wins: 0};
+        r.wins += 1;
+        r.lastWin = x.date.year * 12 + x.date.month;
+        allTime.set(winner, r);
+      }
+    });
+    allTime = Array.from(allTime.values());
+    allTime.sort((a, b) => {
+      let winsDiff = b.wins - a.wins;
+      if (winsDiff !== 0) {
+        return winsDiff;
+      }
+      return a.lastWin - b.lastWin;
+    });
+    return allTime;
+  }
 
-	updateFromStore() {
-		let seasons = LeagueStore.getAll();
-		if (seasons.length == 0) {
-			return;
-		}
-		seasons.reduce((newer, older) => {
-			newer.table.forEach(p => {
-				older.table.forEach((e, i) => {
-					e.globalRank = i;
-				});
-				var sorted = older.table.slice(0).sort((a, b) => {
-					if (a.division !== b.division) {
-						return a.division - b.division;
-					}
-					return a.globalRank - b.globalRank;
-				}); // Sort by division, otherwise given order is fine.
-				let previous = sorted.findIndex(y => y.name === p.name)
-				p.previousRank = previous !== -1 ? previous + 1 : -1;
-			});
-			return older;
-		});
+  updateFromStore() {
+    let seasons = LeagueStore.getAll();
+    if (seasons.length == 0) {
+      return;
+    }
+    seasons.reduce((newer, older) => {
+      newer.table.forEach(p => {
+        older.table.forEach((e, i) => {
+          e.globalRank = i;
+        });
+        var sorted = older.table.slice(0).sort((a, b) => {
+          if (a.division !== b.division) {
+            return a.division - b.division;
+          }
+          return a.globalRank - b.globalRank;
+        }); // Sort by division, otherwise given order is fine.
+        let previous = sorted.findIndex(y => y.name === p.name)
+        p.previousRank = previous !== -1 ? previous + 1 : -1;
+      });
+      return older;
+    });
 
-	  let allTime = this.getTotalWins(seasons, 1);
-		this.setState({
-			data: {
-				seasons: seasons,
-				allTime: {
-					value: allTime.reduce((sum, ele) => sum + ele.wins, 0),
-					label: 'CFL',
-					children: allTime.map(x => ({
-						label: x.name,
-						title: x.name + ' - ' + x.wins,
-						value: x.wins,
-						children: []
-					}))
-				}
-			}
-		});
-	}
+    let allTime = this.getTotalWins(seasons, 1);
+    this.setState({
+      data: {
+        seasons: seasons,
+        allTime: {
+          value: allTime.reduce((sum, ele) => sum + ele.wins, 0),
+          label: 'CFL',
+          children: allTime.map(x => ({
+            label: x.name,
+            title: x.name + ' - ' + x.wins,
+            value: x.wins,
+            children: []
+          }))
+        }
+      }
+    });
+  }
 
   onStoreChange(evt) {
-		this.updateFromStore();
+    this.updateFromStore();
   }
 
   componentDidMount() {
@@ -89,36 +89,36 @@ class League extends Component {
   }
 
   componentWillUnmount() {
-  	this.unsubscribe();
+    this.unsubscribe();
   }
 
   handleSelect(selectedKey) {
-  	this.setState({
-  		tab: selectedKey
-  	});
+    this.setState({
+      tab: selectedKey
+    });
   }
 
-	render() {
-		var content;
-		if (this.state.data === null) {
-			content = <span>Loading...</span>
-		}
-		else if (this.state.tab === 1) {
-			content = <Divisions seasons={this.state.data.seasons} />;
-		}
-		else {
-			content = <AllTime data={this.state.data.allTime}/>;
-		}
-		return (
-		  <Row>
+  render() {
+    var content;
+    if (this.state.data === null) {
+      content = <span>Loading...</span>
+    }
+    else if (this.state.tab === 1) {
+      content = <Divisions seasons={this.state.data.seasons} />;
+    }
+    else {
+      content = <AllTime data={this.state.data.allTime}/>;
+    }
+    return (
+      <Row>
         <Nav bsStyle='tabs' activeKey={this.state.tab} onSelect={key => this.handleSelect(key)}>
           <NavItem eventKey={1}>Table</NavItem>
           <NavItem eventKey={2}>All Time</NavItem>
         </Nav>
-				{content}
+        {content}
       </Row>
-		);
-	}
+    );
+  }
 }
 
 export default League;
